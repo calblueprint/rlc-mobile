698b3f36521c5dec66e9a87553e9505d75d55c60
Wilson Wang <wilsonw926@berkeley.edu>
2019-10-04 15:22:33 -0700

    first signup screen

--- screens/signup/HomeScreen.js
+++ screens/signup/HomeScreen.js
@@ -1,16 +1,18 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
+  Button,
   Image,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
+import { MonoText } from '../../components/StyledText';
 
 export default function HomeScreen() {
   return (
@@ -19,38 +21,21 @@ export default function HomeScreen() {
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
         </View>
 
         <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
+          <Text style={styles.getStartedText}>We're excited to have you join the team! Tell us a little about yourself!</Text>
+          <Text>First Name</Text>
+          <TextInput placeholder={'Jane'}></TextInput>
+          <Text>Last Name</Text>
+          <TextInput placeholder={'Doe'}></TextInput>
+          <Text>Birthday</Text>
+          <TextInput placeholder={'January 1, 2000'}></TextInput>
+          <Button title='NEXT'></Button>
 
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
         </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
 
       <View style={styles.tabBarInfoContainer}>
