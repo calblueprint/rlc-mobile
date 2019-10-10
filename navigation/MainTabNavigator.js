5f80d529f27d523d7752ea5b36ae0a04e0ad7c10
Melody Wei <melodywei861016@gmail.com>
2019-10-09 23:00:47 -0700

    Changed "Login" to "LoginScreen" and removed Switch tag

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -7,7 +7,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 // Testing Login
-import LoginScreen from '../screens/login/Login.js'
+import LoginScreen from '../screens/login/LoginScreen'
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
