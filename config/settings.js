dc2b92f5d397657725f4701b52a2798f56e69a47
Johnathan Zhou <johnathanz@berkeley.edu>
2020-03-02 22:02:54 -0800

    Fixed up env variable problems

--- config/settings.js
+++ config/settings.js
@@ -1,19 +1,21 @@
 import { Platform } from "react-native";
+import { BASE_URL } from 'react-native-dotenv'
+
 
 /**
  * Determines URL based on whether in production or dev environment
  */
 if (process.env.NODE_ENV === "production") {
+  // TODO @Johnathan, work on developing 
+  // the env variables for production.
   console.log("production");
 } else {
   console.log("staging");
   URL = Platform.select({
-    ios: "http://johnathan.dev.calblueprint.org:3000",
-    // For Android Emulator
+    ios: BASE_URL,
     android: "http://10.0.2.2:3000"
   });
 }
-
 export const settings = {
   env: process.env.NODE_ENV,
   URL
