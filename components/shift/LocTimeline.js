5f15bdb0d994c0e459a7bf05327d750abaa9822a
Susan Lin <susan.lin@berkeley.edu>
2020-04-15 17:33:08 -0700

    starting to fix

--- components/shift/LocTimeline.js
+++ components/shift/LocTimeline.js
@@ -9,27 +9,34 @@ export default class ParticipantCard1 extends React.Component {
 
   render() {
     return (
-      <Timeline
-        style={styles.list}
-        data={this.props.markers}
-        circleSize={15}
-        circleColor="#79B830"
-        innerCircle={"dot"}
-        dotColor="#79B830"
-        lineColor="#79B830"
-        titleStyle={{
-          color: "#4A4A4A",
-          fontWeight: "600",
-          fontSize: 17
-        }}
-        descriptionStyle={{
-          color: "#4A4A4A",
-          fontSize: 15,
-          selectable: true
-        }}
-        showTime={false}
-        detailContainerStyle={{ marginTop: -12 }}
-      />
+      <View>
+        <FlatList
+          data={this.props.markers}
+          renderItem={}
+        />
+
+        <Timeline
+          style={styles.list}
+          data={this.props.markers}
+          circleSize={15}
+          circleColor="#79B830"
+          innerCircle={"dot"}
+          dotColor="#79B830"
+          lineColor="#79B830"
+          titleStyle={{
+            color: "#4A4A4A",
+            fontWeight: "600",
+            fontSize: 17
+          }}
+          descriptionStyle={{
+            color: "#4A4A4A",
+            fontSize: 15,
+            selectable: true
+          }}
+          showTime={false}
+          detailContainerStyle={{ marginTop: -12 }}
+        />
+      </View>
     );
   }
 }
