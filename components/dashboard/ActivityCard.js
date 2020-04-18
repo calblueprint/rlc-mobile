d183fa129cbb181c9014be64165c37bca2e167e9
mdterrenal <mariaterrenal91@gmail.com>
2020-04-17 18:26:01 -0700

    Fix shadows for Android devices

--- components/dashboard/ActivityCard.js
+++ components/dashboard/ActivityCard.js
@@ -50,6 +50,7 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: "#fff",
     borderRadius: 5,
+    elevation: 1,
     marginBottom: 10,
     marginLeft: "8%",
     width: "84%",
