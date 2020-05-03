626e56daeeac53a7b0460f475e6e7e28262810f3
Micah Yong <micahtyong@gmail.com>
2020-05-03 12:22:22 -0700

    Small merge conflict

--- components/search/SuggestedEventsList.js
+++ components/search/SuggestedEventsList.js
@@ -9,8 +9,7 @@ import {
 } from "react-native";
 import ActivityCard from "../dashboard/ActivityCard.js";
 import DatePicker from "react-native-datepicker";
-import { getRequest, postRequest } from "../../lib/requests.js"
-
+import { getRequest, postRequest } from "../../lib/requests.js";
 
 import Styles from "../../constants/Styles.js";
 import Sizes from "../../constants/Sizes.js";
@@ -29,114 +28,139 @@ export default class SuggestedEventsList extends Component {
     };
   }
 
-    componentDidMount() {
-        const locations = {};
-        const location_ids = [];
-        for (let i = 0; i < this.props.preferredLocations.length; i++) {
-            const locationId = this.props.preferredLocations[i]["id"];
-            const locationName = this.props.preferredLocations[i]["name"];
-            locations[locationId] = locationName;
-            location_ids.push(locationId);
-        }
-        locations['66']="Astoria";//temporarily forcibly adding Astoria location to user's preferences for testing
-        location_ids.push(66);
-        const times = this.props.preferredTimes;
-        this.setState({ locations: locations, location_ids: location_ids, times: times }, () => { this.processEventData(); });
+  componentDidMount() {
+    const locations = {};
+    const location_ids = [];
+    for (let i = 0; i < this.props.preferredLocations.length; i++) {
+      const locationId = this.props.preferredLocations[i]["id"];
+      const locationName = this.props.preferredLocations[i]["name"];
+      locations[locationId] = locationName;
+      location_ids.push(locationId);
     }
+    locations["66"] = "Astoria"; //temporarily forcibly adding Astoria location to user's preferences for testing
+    location_ids.push(66);
+    const times = this.props.preferredTimes;
     this.setState(
-      {
-        locations: locations,
-      },
+      { locations: locations, location_ids: location_ids, times: times },
       () => {
         this.processEventData();
       }
     );
   }
+  //   this.setState(
+  //     {
+  //       locations: locations,
+  //     },
+  //     () => {
+  //       this.processEventData();
+  //     }
+  //   );
+  // }
 
-    processEventData() {
-        console.log("processing Search");
-        postRequest(
-            `api/search_events`,//${this.state.date.toISOString()}`,
-            responseData => {
-                console.log(responseData);
-                const selectedEventsInDay = [];
-                for (let i = 0; i < responseData.length; i++) {
-                    const currentEvent = {};
-                    const eventDetails = {};
-                    eventDetails["name"] = responseData[i]["title"];
-                    eventDetails["spotsOpen"] = responseData[i]["slot"];
-                    eventDetails["weight"] = responseData[i]["weight"];
-                    eventDetails["numPickups"] = responseData[i]["numPickups"];
-                    const startingTime = new Date(responseData[i]["starting_time"]);
-                    const endingTime = new Date(responseData[i]["ending_time"]);
-                    eventDetails["shiftType"] = ShiftType.searched;
-                    eventDetails["date"] = startingTime.toLocaleDateString("en-US")
-                    eventDetails["start_time"] = startingTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
-                    eventDetails["end_time"] = endingTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
-                    eventDetails["dropoff_locations"] = responseData[i]["dropoff_locations"];
-                    eventDetails["location"] = responseData[i]["location"]
-                    eventDetails["recurring"] = responseData[i]["recurring"]
-                    eventDetails["id"] = responseData[i]["id"]
-                    if (responseData[i]["location_id"] !== null) {
-                        console.log("nullcheck");
-                        const locationIdString = responseData[i]["location_id"].toString();
-                        if (this.state.locations[locationIdString] != null) {
-                            currentEvent["address"] = this.state.locations[locationIdString];
-                            currentEvent["details"] = eventDetails;
-                            selectedEventsInDay.push(currentEvent);
-                        }
-                    }
-                }
-                console.log("finsihed?");
-                console.log(selectedEventsInDay);
-                this.setState({ selectedEventsInDay: selectedEventsInDay }, () => { this.render() });
-            },
-            error => {
-                console.log(error);
-            },
-            {times: this.state.times, location_ids: this.state.location_ids});
-    }
- 
-    renderNewEvents(chosenDate) {
-        postRequest(
-            `api/search_events`,
-            responseData => {
-                const selectedEventsInDay = [];
-                for (let i = 0; i < responseData.length; i++) {
-                    const currentEvent = {};
-                    const eventDetails = {};
-                    eventDetails["name"] = responseData[i]["title"];
-                    eventDetails["spotsOpen"] = responseData[i]["slot"];
-                    eventDetails["weight"] = responseData[i]["pound"] + " lbs";
-                    eventDetails["numPickups"] = responseData[i]["numPickups"];
-                    eventDetails["dropoff_locations"] = responseData[i]["dropoff_locations"];
-                    eventDetails["location"] = responseData[i]["location"]
-                    const startingTime = new Date(responseData[i]["starting_time"]);
-                    const endingTime = new Date(responseData[i]["ending_time"]);
-                    eventDetails["shiftType"] = ShiftType.searched;
-                    eventDetails["date"] = startingTime.toLocaleDateString("en-US")
-                    eventDetails["start_time"] = startingTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
-                    eventDetails["end_time"] = endingTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
-                    eventDetails["recurring"] = responseData[i]["recurring"]
-                    eventDetails["id"] = responseData[i]["id"]
-                    if (responseData[i]["location_id"] != null) {
-                        const locationIdString = responseData[i]["location_id"].toString();
-                        if (this.state.locations[locationIdString] != null) {
-                            currentEvent["address"] = this.state.locations[locationIdString];
-                            currentEvent["details"] = eventDetails;
-                            selectedEventsInDay.push(currentEvent);
-                        }
-                    }
-                }
-                console.log(responseData);
-                console.log("done?");
-                this.setState({ selectedEventsInDay: selectedEventsInDay }, () => { this.render() });
-            },
-            error => {
-                console.log(error);
-            },
-            {date: chosenDate, location_ids: this.state.location_ids});
-    }
+  processEventData() {
+    console.log("processing Search");
+    postRequest(
+      `api/search_events`, //${this.state.date.toISOString()}`,
+      (responseData) => {
+        console.log(responseData);
+        const selectedEventsInDay = [];
+        for (let i = 0; i < responseData.length; i++) {
+          const currentEvent = {};
+          const eventDetails = {};
+          eventDetails["name"] = responseData[i]["title"];
+          eventDetails["spotsOpen"] = responseData[i]["slot"];
+          eventDetails["weight"] = responseData[i]["weight"];
+          eventDetails["numPickups"] = responseData[i]["numPickups"];
+          const startingTime = new Date(responseData[i]["starting_time"]);
+          const endingTime = new Date(responseData[i]["ending_time"]);
+          eventDetails["shiftType"] = ShiftType.searched;
+          eventDetails["date"] = startingTime.toLocaleDateString("en-US");
+          eventDetails["start_time"] = startingTime.toLocaleTimeString(
+            "en-US",
+            { hour: "2-digit", minute: "2-digit" }
+          );
+          eventDetails["end_time"] = endingTime.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          eventDetails["dropoff_locations"] =
+            responseData[i]["dropoff_locations"];
+          eventDetails["location"] = responseData[i]["location"];
+          eventDetails["recurring"] = responseData[i]["recurring"];
+          eventDetails["id"] = responseData[i]["id"];
+          if (responseData[i]["location_id"] !== null) {
+            console.log("nullcheck");
+            const locationIdString = responseData[i]["location_id"].toString();
+            if (this.state.locations[locationIdString] != null) {
+              currentEvent["address"] = this.state.locations[locationIdString];
+              currentEvent["details"] = eventDetails;
+              selectedEventsInDay.push(currentEvent);
+            }
+          }
+        }
+        console.log("finsihed?");
+        console.log(selectedEventsInDay);
+        this.setState({ selectedEventsInDay: selectedEventsInDay }, () => {
+          this.render();
+        });
+      },
+      (error) => {
+        console.log(error);
+      },
+      { times: this.state.times, location_ids: this.state.location_ids }
+    );
+  }
+
+  renderNewEvents(chosenDate) {
+    postRequest(
+      `api/search_events`,
+      (responseData) => {
+        const selectedEventsInDay = [];
+        for (let i = 0; i < responseData.length; i++) {
+          const currentEvent = {};
+          const eventDetails = {};
+          eventDetails["name"] = responseData[i]["title"];
+          eventDetails["spotsOpen"] = responseData[i]["slot"];
+          eventDetails["weight"] = responseData[i]["pound"] + " lbs";
+          eventDetails["numPickups"] = responseData[i]["numPickups"];
+          eventDetails["dropoff_locations"] =
+            responseData[i]["dropoff_locations"];
+          eventDetails["location"] = responseData[i]["location"];
+          const startingTime = new Date(responseData[i]["starting_time"]);
+          const endingTime = new Date(responseData[i]["ending_time"]);
+          eventDetails["shiftType"] = ShiftType.searched;
+          eventDetails["date"] = startingTime.toLocaleDateString("en-US");
+          eventDetails["start_time"] = startingTime.toLocaleTimeString(
+            "en-US",
+            { hour: "2-digit", minute: "2-digit" }
+          );
+          eventDetails["end_time"] = endingTime.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          eventDetails["recurring"] = responseData[i]["recurring"];
+          eventDetails["id"] = responseData[i]["id"];
+          if (responseData[i]["location_id"] != null) {
+            const locationIdString = responseData[i]["location_id"].toString();
+            if (this.state.locations[locationIdString] != null) {
+              currentEvent["address"] = this.state.locations[locationIdString];
+              currentEvent["details"] = eventDetails;
+              selectedEventsInDay.push(currentEvent);
+            }
+          }
+        }
+        console.log(responseData);
+        console.log("done?");
+        this.setState({ selectedEventsInDay: selectedEventsInDay }, () => {
+          this.render();
+        });
+      },
+      (error) => {
+        console.log(error);
+      },
+      { date: chosenDate, location_ids: this.state.location_ids }
+    );
+  }
 
   renderNewEvents(chosenDate) {
     getRequest(
