86b5493d940f7a0f4c5fdb32e8f817f8e3b7e9fc
Micah Yong <micahtyong@gmail.com>
2020-05-04 18:59:38 -0700

    White background color in event and signup

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
 import Sizes from "../constants/Sizes.js";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import DashboardScreen from "../screens/dash/Dashboard2.js";
 import ProfileScreen from "../screens/profile/Profile.js";
@@ -20,40 +20,51 @@ export default class MainScreen extends React.Component {
 
   // Navigate between screens by pressing one of the screens at the bottom tabbar.
   _onSelectNavigationMenu = (index) => () => {
-    this.setState({currentScreenIndex: index})
-  }
+    this.setState({ currentScreenIndex: index });
+  };
 
   //async logoutHandler() To be implemented.
 
   render() {
-    console.log(this.props.navigation)
+    console.log(this.props.navigation);
     return (
-      <View>
-        <View style={{ height: Sizes.height * 0.9, marginTop: Sizes.height * 0.02 }}>
-          {this.state.currentScreenIndex == 0 && <ProfileScreen
-            navigation={this.props.navigation}
-            onNavSelect={this._onSelectNavigationMenu}
-          />}
-          {this.state.currentScreenIndex == 1 && <DashboardScreen
-            navigation={this.props.navigation}
-            onNavSelect={this._onSelectNavigationMenu}
-          />}
-          {this.state.currentScreenIndex == 2 && <SearchScreen
-            navigation={this.props.navigation}
-            onNavSelect={this._onSelectNavigationMenu}
-          />}
-          </View>
+      <View style={styles.MainScreen}>
+        <View
+          style={{ height: Sizes.height * 0.9, marginTop: Sizes.height * 0.02 }}
+        >
+          {this.state.currentScreenIndex == 0 && (
+            <ProfileScreen
+              navigation={this.props.navigation}
+              onNavSelect={this._onSelectNavigationMenu}
+            />
+          )}
+          {this.state.currentScreenIndex == 1 && (
+            <DashboardScreen
+              navigation={this.props.navigation}
+              onNavSelect={this._onSelectNavigationMenu}
+            />
+          )}
+          {this.state.currentScreenIndex == 2 && (
+            <SearchScreen
+              navigation={this.props.navigation}
+              onNavSelect={this._onSelectNavigationMenu}
+            />
+          )}
+        </View>
         <View style={{ height: Sizes.height * 0.08 }}>
-          <NavigationFooter index={this.state.currentScreenIndex} navigationHandler={this._onSelectNavigationMenu} />
+          <NavigationFooter
+            index={this.state.currentScreenIndex}
+            navigationHandler={this._onSelectNavigationMenu}
+          />
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  screenNavigatorScroll: {},
   MainScreen: {
+    backgroundColor: "white",
     flex: 1,
     width: Sizes.width,
   },
