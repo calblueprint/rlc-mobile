18689ed8f0c08fbf00d76a127ef7c5bccb63bd1a
Susan Lin <susan.lin@berkeley.edu>
2020-05-26 11:13:00 -0700

    change num

--- screens/search/Search.js
+++ screens/search/Search.js
@@ -292,12 +292,12 @@ export default class Search extends Component {
     let preferred_locations = await fetch_locations_by_ids(this.state.user.preferred_location_id);
     this.setState({
       locations: preferred_locations.map((item) => ({ ...item, selected: true })),
+      numLocs: preferred_locations.length,
     });
 
     let availability = await fetch_availability();
     this.setState({
       availability: availability,
-      locations: preferred_locations.map((item) => ({ ...item, selected: true })),
       fetchingLoc: false,
     });
   }
